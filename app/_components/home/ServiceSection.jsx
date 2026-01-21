ce31c265007dbae66e3fb93cff40cc78d678019a
varun <varunmathur817@gamil.com>
2026-01-21 11:00:54 +0530

    fix the issue in home page

--- app/_components/home/ServiceSection.jsx
+++ app/_components/home/ServiceSection.jsx
@@ -17,13 +17,18 @@ export default function ServiceSection() {
 
                 <div className="flex flex-col lg:flex-row bg-[#0B5C7A] rounded-2xl overflow-hidden shadow-lg">
                     {/* Left Content */}
-                    <div className="lg:w-7/12 w-full flex items-center justify-center p-8 sm:p-10 md:p-12 lg:p-14 text-white">
+                    <div
+                        className="lg:w-7/12 w-full flex items-center justify-center p-8 sm:p-10 md:p-12 lg:p-14 text-white bg-cover bg-center bg-no-repeat"
+                        style={{
+                            backgroundImage: "url('/static/aboutbg.png')",
+                        }}
+                    >
                         <div className="flex flex-col items-center text-center gap-4 max-w-xl">
-                            <h2 className="md:text-[36px] text-[28px]">
+                            <h2 className="md:text-[36px] text-[28px] font-semibold">
                                 Job Oriented Courses
                             </h2>
 
-                            <p className="md:text-[24px] text-[18px] opacity-90 leading-8">
+                            <p className="md:text-[26px] text-[18px] leading-8 font-semibold">
                                 <span>(Certificate / Diploma / Degree)</span>
                                 <br />
                                 <span className="mt-3 inline-block">
@@ -67,12 +72,16 @@ export default function ServiceSection() {
                             />
                             <div className="absolute inset-0 bg-black/30"></div>
                             {/* Overlay Text */}
-                            <div className="absolute top-8 right-6 md:right-10 flex flex-col items-end text-right max-w-sm">
+                            <div className="absolute top-8 right-6 md:right-10 flex flex-col items-end text-right max-w-md">
                                 <h2 className="text-white font-semibold text-center md:text-[24px] text-[18px] mb-4 leading-tight shadow-sm">
                                     Honorary Doctorates <br /> and D.Litt
                                 </h2>
 
-                                <div className="bg-white text-black py-3 px-5  shadow-lg text-[12px] md:text-[16px] font-medium leading-snug text-center">
+                                <div
+                                    className="bg-white text-black py-3 px-5 shadow-lg 
+                text-[14px] md:text-[16px] font-medium leading-snug text-center
+                w-[257px] md:w-auto mx-auto"
+                                >
                                     Non-Academic Awards for{" "}
                                     <br className="hidden md:block" /> achievers
                                     and eminent persons
@@ -81,84 +90,91 @@ export default function ServiceSection() {
                         </div>
 
                         {/* Right Cards */}
-                       {/* Right Cards */}
-<div className="flex flex-col lg:flex-col gap-4 lg:gap-6 sm:flex-row ">
-    <div className="bg-[#187ba1] text-white rounded-xl p-6 flex flex-col justify-center items-center text-center shadow-lg hover:shadow-xl transition-shadow w-full">
-        <h3 className="md:text-[24px] text-[20px] font-semibold mb-6">
-            Excellence Awards
-        </h3>
+                        {/* Right Cards */}
+                        <div className="flex flex-col lg:flex-col gap-4 lg:gap-6 sm:flex-row ">
+                            <div className="bg-[#187ba1] text-white rounded-xl p-6 flex flex-col justify-center items-center text-center shadow-lg hover:shadow-xl transition-shadow w-full">
+                                <h3 className="md:text-[24px] text-[18px] font-semibold mb-6">
+                                    Excellence Awards
+                                </h3>
 
-        <Link
-            href={"/Excellence"}
-            className="bg-white text-[#012A3C] px-6 py-3 text-sm md:text-[15px] font-semibold hover:bg-gray-50 transition shadow-sm"
-        >
-            Click To Know Award Categories
-        </Link>
-    </div>
+                                <Link
+                                    href={"/Excellence"}
+                                    className="bg-white text-[#012A3C] px-6 py-3 text-[14px] md:text-[16px] font-medium hover:bg-gray-50 transition shadow-sm"
+                                >
+                                    Click To Know Award Categories
+                                </Link>
+                            </div>
 
-    <div className="bg-[#187ba1] text-white rounded-xl p-6 flex flex-col justify-center items-center text-center shadow-lg hover:shadow-xl transition-shadow w-full">
-       <h3 className="md:text-[24px] text-[18px] font-semibold mb-3 leading-tight">
-    Professional Training <br /> Programs
-</h3>
+                            <div className="bg-[#187ba1] text-white rounded-xl p-6 flex flex-col justify-center items-center text-center shadow-lg hover:shadow-xl transition-shadow w-full">
+                                <h3 className="md:text-[24px] text-[18px] font-semibold mb-3 leading-tight">
+                                    Professional Training <br /> Programs
+                                </h3>
 
-
-        <Link
-            href={"/ProgramTraining"}
-            className="bg-white text-[#012A3C] px-8 py-3 text-sm md:text-[15px] font-semibold hover:bg-gray-50 transition shadow-sm"
-        >
-            Click Here
-        </Link>
-    </div>
-</div>
-
+                                <Link
+                                    href={"/ProgramTraining"}
+                                    className="bg-white text-[#012A3C] px-8 py-3 text-[14px] md:text-[16px] font-semibold hover:bg-gray-50 transition shadow-sm"
+                                >
+                                    Click Here
+                                </Link>
+                            </div>
+                        </div>
                     </div>
                     <div className="max-w-7xl mx-auto mt-6">
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-5">
                             {/* First Image */}
-                            <div className="relative overflow-hidden rounded-xl 
-                h-[200px] sm:h-auto shadow-lg">
-    <Image
-        src="/static/school.png"
-        width={600}
-        height={400}
-        alt="school"
-        className="w-full h-full sm:h-auto object-cover 
+                            <div
+                                className="relative overflow-hidden rounded-xl 
+                h-[200px] sm:h-auto shadow-lg"
+                            >
+                                <Image
+                                    src="/static/school.png"
+                                    width={600}
+                                    height={400}
+                                    alt="school"
+                                    className="w-full h-full sm:h-auto object-cover 
                    transform transition-transform duration-300 hover:scale-95"
-    />
+                                />
 
-    <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/25 to-transparent"></div>
+                                <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/25 to-transparent"></div>
 
-    <p className="absolute bottom-2 
+                                <p
+                                    className="absolute bottom-2 
                   text-[18px] md:text-[24px] 
                   leading-6 md:leading-7 
-                  text-white px-4 sm:px-6 py-5">
-        Establishment of Schools/Colleges/Universities & Off-Shore Campuses
-    </p>
-</div>
-
+                  text-white px-4 sm:px-6 py-5"
+                                >
+                                    Establishment of
+                                    Schools/Colleges/Universities & Off-Shore
+                                    Campuses
+                                </p>
+                            </div>
 
                             {/* Second Image */}
-                            <div className="relative overflow-hidden rounded-xl 
-                h-[200px] sm:h-auto shadow-lg">
-    <Image
-        src="/static/laptop.png"
-        width={600}
-        height={400}
-        alt="laptop"
-        className="w-full h-full sm:h-auto object-cover 
+                            <div
+                                className="relative overflow-hidden rounded-xl 
+                h-[200px] sm:h-auto shadow-lg"
+                            >
+                                <Image
+                                    src="/static/laptop.png"
+                                    width={600}
+                                    height={400}
+                                    alt="laptop"
+                                    className="w-full h-full sm:h-auto object-cover 
                    transform transition-transform duration-300 hover:scale-95"
-    />
+                                />
 
-    <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/25 to-transparent"></div>
+                                <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/25 to-transparent"></div>
 
-    <p className="absolute bottom-2 
+                                <p
+                                    className="absolute bottom-2 
                   text-[18px] md:text-[24px] 
                   leading-6 md:leading-7 
-                  text-white px-4 sm:px-6 py-5 mb-2">
-        Twinning, Exchange and dual certification programs
-    </p>
-</div>
-
+                  text-white px-4 sm:px-6 py-5 mb-2"
+                                >
+                                    Twinning, Exchange and dual certification
+                                    programs
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
