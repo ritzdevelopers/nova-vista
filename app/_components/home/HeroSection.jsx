66d0e6b0091e424da46855e0679134f74765a565
varun <varunmathur817@gamil.com>
2026-01-21 19:03:29 +0530

    aa

--- app/_components/home/HeroSection.jsx
+++ app/_components/home/HeroSection.jsx
@@ -2,7 +2,7 @@ import MotionSection from "../motion/MotionSection";
 
 export default function HeroSection() {
   return (
-    <MotionSection className="w-full h-[55vh] min-h-[320px] md:h-[65vh] lg:h-[636px] overflow-hidden mt-20 md:mt-0">
+    <MotionSection className="w-full h-[55vh] min-h-[320px] md:h-[65vh] lg:h-[636px] overflow-hidden ">
       <video
         src="/static/hero-section-video.mp4"
         autoPlay
